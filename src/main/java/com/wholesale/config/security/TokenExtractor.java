bd7b079fc1531687116005193a25f81a219d1d51
Tharindu <tmweeratunga31@gmail.com>
2023-03-08 15:01:35 +1100

    change TokenExtractor to avoid exceptions

--- src/main/java/com/wholesale/config/security/TokenExtractor.java
+++ src/main/java/com/wholesale/config/security/TokenExtractor.java
@@ -5,14 +5,21 @@ import org.springframework.http.HttpHeaders;
 
 import java.nio.file.AccessDeniedException;
 
+/*
+* This is sample implementation and not completed because of login out of scope
+*/
+
 @Configuration
 public class TokenExtractor {
 
     public static String getUserIdFromHeader(HttpHeaders httpHeaders) throws AccessDeniedException {
         String token = httpHeaders.getFirst("authorization");
-        if(token == null || token.isEmpty()){
-            throw new AccessDeniedException("Invalid authorization");
+        if(token != null && !token.isEmpty()){
+            String[] tokenData = token.split(" ");
+            if(tokenData.length == 2) {
+                return token.split(" ")[1];
+            }
         }
-        return token.split(" ")[1];
+        throw new AccessDeniedException("Invalid authorization");
     }
 }
